c04ee0302aaf856d8919dd272f5d881c8b46028e
Richard Rispoli <rrispoli@hibernum.com>
2014-02-23 15:29:12 -0500

    init

---
+++ Assets/scripts/BG_Token.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BG_Token : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
