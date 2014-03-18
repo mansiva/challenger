a84adcb09f4ba35dae822e0144bb6a12adf30f40
Richard Rispoli <rrispoli@hibernum.com>
2014-03-18 07:59:11 -0400

    Importing Dice Animation

---
+++ Assets/Playtest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playtest : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+		//animator.SetTrigger("throw_dice");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
